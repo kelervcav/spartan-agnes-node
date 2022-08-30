aed1806ce7f19db26f5a6175461807dd692d1542
ibsonpalinlin <ibsonpalinlin@gitlab.com>
2022-08-30 09:13:41 +0800

    Add test devices file

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -4,10 +4,6 @@ from main import api
 
 client = TestClient(api)
 
-def test_can_fetch_devices():
-    response = client.get("/api/devices")
-    assert response.status_code == 200
-
 def test_can_store_device():
     response = client.post(
         "/api/devices",
@@ -23,4 +19,32 @@ def test_can_store_device():
         },
         "status": 200      
     }
+
+def test_can_fetch_devices():
+    response = client.get("/api/devices")
+    assert response.status_code == 200
+    
+def test_can_fetch_device():
+    response = client.get("/api/devices/1")
+    assert response.status_code == 200
+
+def test_can_update_device():
+    response = client.put(
+        "/api/devices/1",
+        # headers={"X-Token": "coneofsilence"},
+        json={"name": "testing", "unit": 100, "address": 25},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "data": {
+            "name": "testing",
+            "unit": 100,
+            "address": 25
+        },
+        "status": 200      
+    }
+
+def test_can_delete_device():
+    response = client.delete("/api/devices/1")
+    assert response.status_code == 200
 
