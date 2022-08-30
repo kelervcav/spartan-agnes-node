d04fa0909b8d78ecdb555577d7bd96e679517f42
ibsonpalinlin <ibsonpalinlin@gitlab.com>
2022-08-30 08:30:42 +0800

    wip

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -7,3 +7,20 @@ client = TestClient(api)
 def test_can_fetch_devices():
     response = client.get("/api/devices")
     assert response.status_code == 200
+
+def test_can_store_device():
+    response = client.post(
+        "/api/devices",
+        # headers={"X-Token": "coneofsilence"},
+        json={"name": "foobar", "unit": 1, "address": 2},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "data": {
+            "name": "foobar",
+            "unit": 1,
+            "address": 2
+        },
+        "status": 200      
+    }
+
