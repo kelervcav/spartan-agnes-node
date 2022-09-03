ecd78a0e979af56083101301cbcdfcb0d8aa6264
Sydel Palinlin <sydel.palinlin@gmail.com>
2022-09-03 21:51:19 +0800

    wip

--- migrations/env.py
+++ migrations/env.py
@@ -5,13 +5,15 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-import app.devices.models as models
+import app.devices.models as devices_model
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 fileConfig(config.config_file_name)
-target_metadata = models.Base.metadata
+target_metadata = [
+     devices_model.Base.metadata
+]
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
