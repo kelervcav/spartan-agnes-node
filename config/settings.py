9ac90ec5dc1cf3a734a180aa41bcaabc233fc1aa
Sydel Palinlin <sydel.palinlin@gmail.com>
2022-09-10 05:52:47 +0800

    wip

--- config/settings.py
+++ config/settings.py
@@ -7,5 +7,3 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = 'utf-8'
-
-# settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
