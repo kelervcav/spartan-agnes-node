8f1944066bc2d607909a958587b042f8d67f4b47
Sydel Palinlin <sydel.palinlin@gmail.com>
2022-09-09 22:25:04 +0800

    wip

--- app/commands/api.py
+++ app/commands/api.py
@@ -3,6 +3,7 @@ sys.path.append("..")
 
 from fastapi import Depends, HTTPException, APIRouter
 from .models import Commands
+from ..devices.models import Devices
 from config.database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
@@ -15,10 +16,12 @@ def get_db():
         db.close()
 
 class Command(BaseModel):
-    name: str
     device_id: int
-    address: str
-    value: str
+
+class Device(BaseModel):
+    name: str
+    unit: int
+    address: int
 
 router = APIRouter(
     prefix="/api",
@@ -26,40 +29,44 @@ router = APIRouter(
     responses={404: {"description": "Resource not found"}}
 )
 
-@router.get('/commands/info')
+@router.post('/commands/info')
 async def fetch_info_command(command: Command, db: Session=Depends(get_db)):
-    data = Commands()
 
-    data.device_id = command.device_id
-    data.unit = command.unit
-    data.address = command.address
-    data.value = command.value
-
-    db.add(data)
-    db.commit()
+    # Get device info
+    data = db.query(Devices)\
+                    .filter(Devices.id == command.device_id)\
+                    .first()
 
     # Hardware communication here
 
+    # Get result value
+    get_this_value = 30
+
     return {
-        'data': command,
+        'data': {
+            'address': data.address,
+            'unit': data.unit,
+            'value': get_this_value
+        },
         'status': 200
     }
 
 @router.post('/commands/work')
 async def do_work_command(command: Command, db: Session=Depends(get_db)):
-    data = Commands()
 
-    data.device_id = command.device_id
-    data.unit = command.unit
-    data.address = command.address
-    data.value = command.value
+      # Get device info
+    data = db.query(Devices)\
+                    .filter(Devices.id == command.device_id)\
+                    .first()
 
-    db.add(data)
-    db.commit()
-
-    # Hardware communication here
+    # Set value to the device
+    set_this_value = True
 
     return {
-        'data': command,
+        'data': {
+            'address': data.address,
+            'unit': data.unit,
+            'value': set_this_value
+        },
         'status': 200
     }
