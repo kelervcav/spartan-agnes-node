4894effe26d4faa40e61f41fdccff336aae982bd
Sydel Palinlin <sydel.palinlin@gmail.com>
2022-09-09 21:52:07 +0800

    wip

--- app/commands/api.py
+++ app/commands/api.py
@@ -26,82 +26,40 @@ router = APIRouter(
     responses={404: {"description": "Resource not found"}}
 )
 
-@router.get('/commands')
-async def list_commands(db: Session=Depends(get_db)):
-    commands = db.query(Commands).all()
-    return {
-        'data': commands,
-        'status': 200
-    }
-
-@router.post('/commands')
+@router.get('/commands/info')
 async def store_command(command: Command, db: Session=Depends(get_db)):
     data = Commands()
-    data.name = command.name
+
+    data.device_id = command.device_id
+    data.unit = command.unit
     data.address = command.address
     data.value = command.value
-    data.device_id = command.device_id
 
     db.add(data)
     db.commit()
+
+    # Hardware communication here
 
     return {
         'data': command,
         'status': 200
     }
 
-@router.get('/commands/{id}')
-async def show_command(id: int, db: Session=Depends(get_db)):
-    command = db.query(Commands)\
-                    .filter(Commands.id == id)\
-                    .first()
+@router.post('/commands/work')
+async def store_command(command: Command, db: Session=Depends(get_db)):
+    data = Commands()
 
-    if command is not None:
-        return {
-            'data': command,
-            'status': 200
-        }
-    else:
-        raise HTTPException(status_code=404, detail="Command not found")
-
-@router.put('/commands/{id}')
-async def update_command(id: int, command: Command, db: Session=Depends(get_db)):
-    data = db.query(Commands)\
-                    .filter(Commands.id == id)\
-                    .first()
-
-    if command is None:
-        raise HTTPException(status_code=404, detail="Command not found")
-
-    data.name = command.name
+    data.device_id = command.device_id
+    data.unit = command.unit
     data.address = command.address
     data.value = command.value
-    data.device_id = command.device_id
-
 
     db.add(data)
     db.commit()
 
+    # Hardware communication here
+
     return {
         'data': command,
-        'status': 200
-    }
-
-@router.delete('/commands/{id}')
-async def delete_command(id: int, db: Session=Depends(get_db)):
-    command = db.query(Commands)\
-                    .filter(Commands.id == id)\
-                    .first()
-
-    if command is None:
-        raise HTTPException(status_code=404, detail="Command not found")
-
-    db.query(Commands)\
-        .filter(Commands.id == id)\
-        .delete()
-
-    db.commit()
-
-    return {
         'status': 200
     }
